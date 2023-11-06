c95ff3dc547a3d7a5a36d528e42f158c4676d992
Jesse Chong <jessechong@pursuit.org>
2023-11-06 17:58:05 -0500

    Basic vite react start

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+// DEPENDENCIES
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+// PAGES
+
+// COMPONENTS
+
+function App() {
+    return (
+        <div className="App">
+            <p>Hello world</p>
+        </div>
+    )
+}
+
+export default App;
